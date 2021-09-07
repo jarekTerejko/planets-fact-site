a795180b1c93b54e39b79872bbd843f02432928a
jarekTerejko <j.terejko@gmail.com>
2021-09-07 11:56:04 +0200

    add Navbar

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,186 @@
+import styled from "styled-components";
+import { Link, NavLink } from "react-router-dom";
+
+export const NavbarWrapper = styled.nav`
+  position: relative;
+  width: 100%;
+  transition: background-color var(--transition);
+  background: ${(props) => (props.$isOpen ? "var(--color-black)" : "")};
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    left: 0;
+    bottom: 0;
+    background: var(--color-dark-grey);
+  }
+`;
+
+export const NavbarInnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.2rem 3.5rem;
+  transition: padding var(--transition);
+
+  @media screen and (max-width: 1030px) {
+    padding: 3.5rem;
+    flex-direction: column;
+  }
+
+  @media screen and (max-width: 830px) {
+    flex-direction: row;
+    padding: 1.2rem 2.5rem;
+  }
+`;
+
+export const NavbarSiteLinkR = styled(Link)`
+  text-decoration: none;
+`;
+
+export const NavbarLogo = styled.span`
+  font-family: var(--ff-oswald);
+  text-transform: uppercase;
+  font-size: 2.8rem;
+  font-weight: 400;
+  color: var(--color-white);
+  display: block;
+
+  @media screen and (max-width: 1030px) {
+    margin-bottom: 2rem;
+  }
+  @media screen and (max-width: 830px) {
+    margin-bottom: 0;
+    font-size: 2.4rem;
+  }
+`;
+
+export const NavbarMenuBtn = styled.button`
+  background: none;
+  border: none;
+  width: 4rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  display: none;
+
+  @media screen and (max-width: 830px) {
+    transform: translateX(10px);
+    display: flex;
+  }
+`;
+
+export const NavbarNavItems = styled.ul`
+  display: flex;
+  list-style: none;
+  transform: translateX(20px);
+  transition: var(--transition);
+
+  @media screen and (max-width: 1030px) {
+    transform: translateX(0);
+  }
+
+  @media screen and (max-width: 830px) {
+    position: absolute;
+    transform: ${(props) =>
+      props.$isOpen ? `translateX(0)` : "translateX(-100%)"};
+    left: 0;
+    top: 59px;
+    flex-direction: column;
+    background-color: var(--color-black);
+    width: 100%;
+    min-height: calc(100vh - 59px);
+  }
+`;
+
+export const NavbarNavItem = styled.li`
+  margin: 2rem 1rem;
+
+  @media screen and (max-width: 830px) {
+    margin: 1rem 2.5rem;
+  }
+`;
+
+export const NavbarNavLinkR = styled(NavLink)`
+  padding: 1.3rem 1rem 1rem;
+  font-size: 1.2rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: var(--color-light-grey);
+  transition: color var(--transition);
+  text-decoration: none;
+  font-weight: 700;
+  position: relative;
+
+  &:hover {
+    color: var(--color-white);
+  }
+
+  &.active {
+    color: red;
+  }
+
+  @media screen and (max-width: 830px) {
+    font-size: 1.4rem;
+    padding-left: 5rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 10px;
+      left: 0;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+
+      &:nth-of-type(1) {
+        background-color: var(--color-venus);
+      }
+      &:nth-of-type(2) {
+        background-color: var(--color-mercury);
+      }
+    }
+
+    &.mercury::before {
+      background-color: var(--color-mercury);
+    }
+    &.venus::before {
+      background-color: var(--color-venus);
+    }
+    &.earth::before {
+      background-color: var(--color-earth);
+    }
+    &.mars::before {
+      background-color: var(--color-mars);
+    }
+    &.jupiter::before {
+      background-color: var(--color-jupiter);
+    }
+    &.saturn::before {
+      background-color: var(--color-saturn);
+    }
+    &.uranus::before {
+      background-color: var(--color-uranus);
+    }
+    &.neptune::before {
+      background-color: var(--color-neptune);
+    }
+  }
+`;
+
+
+export const NavbarChevronWrapper = styled.div`
+  display: none;
+
+  @media screen and (max-width: 830px) {
+    display: block;
+  }
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  NavbarWrapper,
+  NavbarInnerWrapper,
+  NavbarSiteLinkR,
+  NavbarMenuBtn,
+  NavbarNavItems,
+  NavbarNavItem,
+  NavbarNavLinkR,
+  NavbarLogo,
+  NavbarChevronWrapper,
+} from "./NavbarElements";
+import Bars from "../../images/menu.svg";
+import X from "../../images/close.svg";
+import Chevron from "../../images/icon-chevron.svg";
+import { ImageEl } from "../Image/ImageElement";
+
+const Navbar = ({ isOpen, handleMenu }) => {
+
+  const closeMenu = () => {
+    if (isOpen) {
+      handleMenu();
+    } else {
+      return;
+    }
+  };
+
+  const planets = [
+    "mercury",
+    "venus",
+    "earth",
+    "mars",
+    "jupiter",
+    "saturn",
+    "uranus",
+    "neptune",
+  ];
+
+  return (
+    <NavbarWrapper $isOpen={isOpen}>
+      <NavbarInnerWrapper>
+        <NavbarSiteLinkR to="/" $isOpen={isOpen} onClick={closeMenu}>
+          <NavbarLogo>The Planets</NavbarLogo>
+        </NavbarSiteLinkR>
+        <NavbarMenuBtn onClick={handleMenu}>
+          <ImageEl
+            src={isOpen ? `${X}` : `${Bars}`}
+            alt={isOpen ? "Close Menu" : "Open Menu"}
+          />
+        </NavbarMenuBtn>
+        <NavbarNavItems $isOpen={isOpen}>
+          {planets.map((planet, i) => {
+            return (
+              <NavbarNavItem>
+                <NavbarNavLinkR
+                  to={`/${planet}`}
+                  key={i}
+                  $isOpen={isOpen}
+                  onClick={closeMenu}
+                  className={`${planet}`}
+                  // exact={true}
+                >
+                  {planet}
+                  <NavbarChevronWrapper>
+                    <ImageEl src={Chevron} alt="" />
+                  </NavbarChevronWrapper>
+                </NavbarNavLinkR>
+              </NavbarNavItem>
+            );
+          })}
+        </NavbarNavItems>
+      </NavbarInnerWrapper>
+    </NavbarWrapper>
+  );
+};
+
+export default Navbar;
