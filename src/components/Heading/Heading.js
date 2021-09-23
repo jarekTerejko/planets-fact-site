48afe5db73f450ef039e3de0a5d5fd32a3bcf56d
jarekTerejko <j.terejko@gmail.com>
2021-09-23 08:48:31 +0200

    add Heading Element

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Heading = styled.h1`
+  font-size: 6rem;
+  font-family: var(--ff-oswald);
+  text-transform: uppercase;
+  font-weight: 400;
+  margin-bottom: 1rem;
+`;
