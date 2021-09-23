51f8d5d48dd56208ff3a9118fd636f39bd26bc9d
jarekTerejko <j.terejko@gmail.com>
2021-09-23 08:14:12 +0200

    import pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import Home from "./pages/Homepage/Home";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Planet from "./pages/Planet/Planet";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      planets
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/:name" exact>
+          <Planet />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
